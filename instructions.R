40e568778c32f0c4874c66476e6583cb2d735c2b
fay <108646261+thalassa0@users.noreply.github.com>
2022-07-08 09:33:10 +1000

    Update instructions.R
    adding instructions

--- instructions.R
+++ instructions.R
@@ -4,5 +4,6 @@
 # resolved
 #https://docs.github.com/en/authentication/connecting-to-github-with-ssh/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent
 #https://docs.github.com/en/authentication/connecting-to-github-with-ssh/adding-a-new-ssh-key-to-your-github-account
+#https://stackoverflow.com/questions/12940626/github-error-message-permission-denied-publickey
 
 #new test
